4541f86d3b95d4e53932095adf837fceddc534ab
ShanksFD <anassookrat@gmail.com>
2023-11-01 01:10:10 +0100

    Fix issues

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,245 +1,250 @@
 import {
-   USER_LOGIN_FAILED,
-   USER_LOGIN_REQUEST,
-   USER_LOGIN_SUCCESS,
-   
-   USER_LOGOUT_SUCCESS,
-   USER_LOGOUT_FAILED,
-
-   USER_LIST_FAILED,
-   USER_LIST_REQUEST,
-   USER_LIST_SUCCESS,
-
-   USER_REGISTER_FAILED,
-   USER_REGISTER_REQUEST,
-   USER_REGISTER_SUCCESS,
-
-   USER_DETAILS_FAILED,
-   USER_DETAILS_REQUEST,
-   USER_DETAILS_SUCCESS,
-
-   USER_UPDATE_PROFILE_FAILED,
-   USER_UPDATE_PROFILE_REQUEST,
-   USER_UPDATE_PROFILE_SUCCESS,
-
-   USER_DELETE_REQUEST,
-   USER_DELETE_FAILED,
-   USER_DELETE_SUCCESS,
-
-   USER_MANAGER_LIST_REQUEST,
-   USER_MANAGER_LIST_FAILED,
-   USER_MANAGER_LIST_SUCCESS
+  USER_LOGIN_FAILED,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT_SUCCESS,
+  USER_LOGOUT_FAILED,
+  USER_LIST_FAILED,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_REGISTER_FAILED,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_DETAILS_FAILED,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_UPDATE_PROFILE_FAILED,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_DELETE_REQUEST,
+  USER_DELETE_FAILED,
+  USER_DELETE_SUCCESS,
+  USER_MANAGER_LIST_REQUEST,
+  USER_MANAGER_LIST_FAILED,
+  USER_MANAGER_LIST_SUCCESS,
 } from "../constants/userConstants";
-import firebase, {auth} from "../firebase"
+import firebase, { auth } from "../firebase";
 
 export const login = (email, password) => async (dispatch) => {
-   try {
-      dispatch({
-         type: USER_LOGIN_REQUEST,
-      });
-      
-      // Login user
-      const authData = await auth.signInWithEmailAndPassword(email, password);
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-      // grab and assign added user
-      const docRef = firebase.collection("users").doc(authData.user.uid)
-      const doc = await docRef.get();
-      const data = doc.data();
+    // Login user
+    const authData = await auth.signInWithEmailAndPassword(email, password);
 
-      dispatch({
-         type: USER_LOGIN_SUCCESS,
-         payload: data,
-      });
+    // grab and assign added user
+    const docRef = firebase.collection("users").doc(authData.user.uid);
+    const doc = await docRef.get();
+    const data = doc.data();
 
-      localStorage.setItem("userInfo", JSON.stringify(data));
-   } catch (error) {
-      dispatch({
-         type: USER_LOGIN_FAILED,
-         payload: error.message,
-      });
-   }
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAILED,
+      payload: error.message,
+    });
+  }
 };
 
 export const logout = () => async (dispatch) => {
-   try
-   {
-      await auth.signOut();
+  try {
+    await auth.signOut();
 
-      localStorage.removeItem("userInfo");
-      dispatch({
-         type: USER_LOGOUT_SUCCESS
-      })
-   } catch(error) {
-      dispatch({
-         type: USER_LOGOUT_FAILED,
-         payload:
-            error.response && error.response.data.detail
-               ? error.response.data.detail
-               : error.message,
-      });
-   }
-}
+    localStorage.removeItem("userInfo");
+    dispatch({
+      type: USER_LOGOUT_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_LOGOUT_FAILED,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
 
 export const listUsers = () => async (dispatch) => {
-   try {
-      dispatch({
-         type: USER_LIST_REQUEST,
-      });
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    });
 
-      const snapshot = await firebase.collection("users").get()
-      const data = snapshot.docs.map(doc => doc.data());
-      
-      dispatch({
-         type: USER_LIST_SUCCESS,
-         payload: data,
-      });
+    const snapshot = await firebase.collection("users").get();
+    const data = snapshot.docs.map((doc) => doc.data());
 
-   } catch (error) {
-      dispatch({
-         type: USER_LIST_FAILED,
-         payload:
-            error.response && error.response.data.detail
-               ? error.response.data.detail
-               : error.message,
-      });
-   }
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_LIST_FAILED,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
 };
 
-export const register = (first_name, last_name, email, password, phone_number, 
-                        admin, director, projectManager, accountingManager, secretary) => async (dispatch) => {
-   try {
+export const register =
+  (
+    first_name,
+    last_name,
+    email,
+    password,
+    phone_number,
+    admin,
+    director,
+    projectManager,
+    accountingManager,
+    secretary
+  ) =>
+  async (dispatch) => {
+    try {
       dispatch({
-         type: USER_REGISTER_REQUEST,
+        type: USER_REGISTER_REQUEST,
       });
 
-      const authData = await auth.createUserWithEmailAndPassword(email, password);
-      
+      const authData = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
       var user = {
-         uid: authData.user.uid,
-         first_name: first_name,
-         last_name: last_name,
-         phone_number: phone_number,
-         is_admin: admin,
-         is_director: director,
-         is_accountingManager: accountingManager,
-         is_projectManager: projectManager,
-         is_secretary: secretary,
-         email: authData.user.email
-      }
-      
+        uid: authData.user.uid,
+        first_name: first_name,
+        last_name: last_name,
+        phone_number: phone_number,
+        is_admin: admin,
+        is_director: director,
+        is_accountingManager: accountingManager,
+        is_projectManager: projectManager,
+        is_secretary: secretary,
+        email: authData.user.email,
+      };
+
       // Add user details
       await firebase.collection("users").doc(authData.user.uid).set(user);
-      
+
       // grab and assign added user
-      const docRef = firebase.collection("users").doc(authData.user.uid)
+      const docRef = firebase.collection("users").doc(authData.user.uid);
       const doc = await docRef.get();
       const data = doc.data();
 
       dispatch({
-         type: USER_REGISTER_SUCCESS,
-         payload: data,
+        type: USER_REGISTER_SUCCESS,
+        payload: data,
       });
-   } catch (error) {
+    } catch (error) {
       dispatch({
-         type: USER_REGISTER_FAILED,
-         payload:
-            error.response && error.response.data.detail
-               ? error.response.data.detail
-               : error.message,
+        type: USER_REGISTER_FAILED,
+        payload:
+          error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
       });
-   }
-};
+    }
+  };
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
-   try {
-      dispatch({
-         type: USER_DETAILS_REQUEST,
-      });
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
 
-      // grab and assign added user
-      const docRef = await firebase.collection("users").doc(id)
-      const doc = await docRef.get();
-      const data = doc.data();
+    // grab and assign added user
+    const docRef = await firebase.collection("users").doc(id);
+    const doc = await docRef.get();
+    const data = doc.data();
 
-      dispatch({
-         type: USER_DETAILS_SUCCESS,
-         payload: data,
-      });
-
-   } catch (error) {
-      dispatch({
-         type: USER_DETAILS_FAILED,
-         payload: error.message,
-      });
-   }
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAILED,
+      payload: error.message,
+    });
+  }
 };
 
 export const updateUserProfile = (user) => async (dispatch) => {
-   try {
-      dispatch({
-         type: USER_UPDATE_PROFILE_REQUEST,
-      });
-      
-      await firebase.collection("users").doc(user.uid).update(user);
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    });
 
-      dispatch({
-         type: USER_UPDATE_PROFILE_SUCCESS,
-      });
+    await firebase.collection("users").doc(user.uid).update(user);
 
-   } catch (error) {
-      dispatch({
-         type: USER_UPDATE_PROFILE_FAILED,
-         payload:
-            error.response && error.response.data.detail
-               ? error.response.data.detail
-               : error.message,
-      });
-   }
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAILED,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
 };
 
 export const deleteUser = (id) => async (dispatch) => {
-   try
-   {
-      dispatch({
-         type: USER_DELETE_REQUEST,
-         loading: true
-      })
+  try {
+    dispatch({
+      type: USER_DELETE_REQUEST,
+      loading: true,
+    });
 
-      await firebase.collection("users").doc(id).delete()
+    await firebase.collection("users").doc(id).delete();
 
-      dispatch({
-         type: USER_DELETE_SUCCESS,
-         success: true
-      })
-   } catch(error) {
-      dispatch({
-         type: USER_DELETE_FAILED,
-         payload:
-            error.response && error.response.data.detail
-               ? error.response.data.detail
-               : error.message,
-      });
-   }
-}
+    dispatch({
+      type: USER_DELETE_SUCCESS,
+      success: true,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DELETE_FAILED,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
 
 export const getProjectManagerList = () => async (dispatch) => {
-   try {
-      dispatch({
-         type: USER_MANAGER_LIST_REQUEST,
-      });
+  try {
+    dispatch({
+      type: USER_MANAGER_LIST_REQUEST,
+    });
 
-      const snapshot = await firebase.collection("users").where("is_projectManager", "==", true).get()
-      const data = snapshot.docs.map(doc => doc.data());
+    const snapshot = await firebase
+      .collection("users")
+      .where("is_projectManager", "==", true)
+      .get();
+    const data = snapshot.docs.map((doc) => doc.data());
 
-      dispatch({
-         type: USER_MANAGER_LIST_SUCCESS,
-         payload: data
-      });
-
-   } catch (error) {
-      dispatch({
-         type: USER_MANAGER_LIST_FAILED,
-         payload: error.message,
-      });
-   }
+    dispatch({
+      type: USER_MANAGER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_MANAGER_LIST_FAILED,
+      payload: error.message,
+    });
+  }
 };
--- src/screens/NewPhaseScreen.jsx
+++ src/screens/NewPhaseScreen.jsx
@@ -1,135 +1,176 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Form, Button, Col, Row } from "react-bootstrap";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
 // Local Imports
-import FormContainer from '../components/FormContainer';
-import {getProjectDetails, updateProjectPhase} from "../actions/projectActions"
+import FormContainer from "../components/FormContainer";
+import {
+  getProjectDetails,
+  updateProjectPhase,
+} from "../actions/projectActions";
 
-function NewPhaseScreen({history, match}) {
-   const projectDetails = useSelector(state => state.projectDetails)
-   const dispatch = useDispatch();
+function NewPhaseScreen({ history, match }) {
+  const projectDetails = useSelector((state) => state.projectDetails);
+  const dispatch = useDispatch();
 
-   const [label, setLabel] = useState("")
-   const [desc, setDesc] = useState("")
-   const [employees, setEmployees] = useState("")
-   const [startDate, setStartDate] = useState("")
-   const [endDate, setEndDate] = useState("")
-   const [price, setPrice] = useState(0)
+  const [label, setLabel] = useState("");
+  const [desc, setDesc] = useState("");
+  const [employees, setEmployees] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [price, setPrice] = useState(0);
 
-   const submitHandler = (e) => {
-      e.preventDefault()
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-      dispatch(updateProjectPhase(
-         {
-            label: label,
-            description: desc,
-            employees: employees,
-            start_date: startDate,
-            end_date: endDate,
-            price: price,
-            is_done: false
-         },
-         projectDetails.project._id,
-         projectDetails.project.phases
-      ))
-      history.push("/")
-   } 
-   
-   useEffect(() => { 
-      dispatch(getProjectDetails(match.params.id))
-   }, [match, dispatch])
+    dispatch(
+      updateProjectPhase(
+        {
+          label: label,
+          description: desc,
+          employees: employees,
+          start_date: startDate,
+          end_date: endDate,
+          price: price,
+          is_done: false,
+        },
+        projectDetails.project._id,
+        projectDetails.project.phases
+      )
+    );
+    history.push("/");
+  };
 
-   const calculatePhasesPrice = () => {
-      let sum = 0;
-      if(projectDetails.success)
-      {
-         for(let i = 0; i < projectDetails.project.phasesObjs.length; i++)
-         {
-            sum += projectDetails.project.phasesObjs[i].price
-         }
-      }
-      return sum;
-   } 
-   
-   return (
-      <FormContainer>
-         <h1 className="font--light text-center">NEW PHASE</h1>
-         <Form onSubmit={submitHandler}>
-            <Row>
-               <Col>
-                  <Form.Group controlId="label" className="my-3">
-                     <Form.Label>LABEL</Form.Label>
-                     <Form.Control required type="text" placeholder="Phase label" value={label} onChange={ e =>
-                     {
-                        setLabel(e.target.value)
-                     }}/>
-                  </Form.Group>
-               </Col>
+  useEffect(() => {
+    dispatch(getProjectDetails(match.params.id));
+  }, [match, dispatch]);
 
-            </Row>
+  //   const calculatePhasesPrice = () => {
+  //     let sum = 0;
+  //     if (projectDetails.success) {
+  //       for (let i = 0; i < projectDetails.project.phasesObjs.length; i++) {
+  //         sum += projectDetails.project.phasesObjs[i].price;
+  //       }
+  //     }
+  //     return sum;
+  //   };
 
-            <Row>
-               <Form.Group controlId="description" className="my-3">
-                  <Form.Label>DESCRIPTION</Form.Label>
-                  <Form.Control required as="textarea" rows={2} placeholder="Phase description" value={desc} onChange={ e =>
-                  {
-                     setDesc(e.target.value)
-                  }}/>
-               </Form.Group>
-            </Row>
-            
-            <Row>
-               <Form.Group controlId="description" className="my-3">
-                  <Form.Label>EMPLOYEES</Form.Label>
-                  <Form.Control required as="textarea" rows={2} placeholder="Empolyees" value={employees} onChange={ e =>
-                  {
-                     setEmployees(e.target.value)
-                  }}/>
-               </Form.Group>
-            </Row>
+  return (
+    <FormContainer>
+      <h1 className="font--light text-center">NEW PHASE</h1>
+      <Form onSubmit={submitHandler}>
+        <Row>
+          <Col>
+            <Form.Group controlId="label" className="my-3">
+              <Form.Label>LABEL</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Phase label"
+                value={label}
+                onChange={(e) => {
+                  setLabel(e.target.value);
+                }}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
 
-            <Row>
-              <Col lg={6} md={6} sm={12}>
-                  <Form.Group controlId="startDate" className="my-3">
-                     <Form.Label>START DATE</Form.Label>
-                     <Form.Control required type="date" min={projectDetails.project.start_date || 0} max={projectDetails.project.end_date || 0} 
-                        value={startDate} onChange={ e =>
-                        {
-                           setStartDate(e.target.value)
-                        }}/>
-                  </Form.Group>
-               </Col>
+        <Row>
+          <Form.Group controlId="description" className="my-3">
+            <Form.Label>DESCRIPTION</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={2}
+              placeholder="Phase description"
+              value={desc}
+              onChange={(e) => {
+                setDesc(e.target.value);
+              }}
+            />
+          </Form.Group>
+        </Row>
 
-               <Col lg={6} md={6} sm={12}>
-                  <Form.Group controlId="endDate" className="my-3">
-                     <Form.Label>END DATE</Form.Label>
-                     <Form.Control required type="date" min={projectDetails.project.start_date || 0} max={projectDetails.project.end_date || 0}
-                        value={endDate} onChange={ e =>
-                        {
-                           setEndDate(e.target.value)
-                        }}/>
-                  </Form.Group>
-               </Col>
-            </Row>
+        <Row>
+          <Form.Group controlId="description" className="my-3">
+            <Form.Label>EMPLOYEES</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={2}
+              placeholder="Empolyees"
+              value={employees}
+              onChange={(e) => {
+                setEmployees(e.target.value);
+              }}
+            />
+          </Form.Group>
+        </Row>
 
-            <Row>
-               <Form.Group controlId="price" className="my-4">
-                  <Form.Label style={{display: "block"}}>PRICE 
-                     <strong style={{color: "green"}}> (total: ${(Number(projectDetails.project.price)) || 0})</strong> 
-                  </Form.Label>
-                  <Form.Control required type="number" placeholder="Price" min="0" max={(projectDetails.project.price) || 0}
-                     value={price} onChange={ e =>
-                     {
-                        setPrice(e.target.value)
-                     }}/>
-               </Form.Group>
-            </Row>
-            
-            <Button type="submit" variant="primary" className="my-2">SAVE</Button>
-         </Form>
-      </FormContainer>
-   )
+        <Row>
+          <Col lg={6} md={6} sm={12}>
+            <Form.Group controlId="startDate" className="my-3">
+              <Form.Label>START DATE</Form.Label>
+              <Form.Control
+                required
+                type="date"
+                min={projectDetails.project.start_date || 0}
+                max={projectDetails.project.end_date || 0}
+                value={startDate}
+                onChange={(e) => {
+                  setStartDate(e.target.value);
+                }}
+              />
+            </Form.Group>
+          </Col>
+
+          <Col lg={6} md={6} sm={12}>
+            <Form.Group controlId="endDate" className="my-3">
+              <Form.Label>END DATE</Form.Label>
+              <Form.Control
+                required
+                type="date"
+                min={projectDetails.project.start_date || 0}
+                max={projectDetails.project.end_date || 0}
+                value={endDate}
+                onChange={(e) => {
+                  setEndDate(e.target.value);
+                }}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Form.Group controlId="price" className="my-4">
+            <Form.Label style={{ display: "block" }}>
+              PRICE
+              <strong style={{ color: "green" }}>
+                {" "}
+                (total: ${Number(projectDetails.project.price) || 0})
+              </strong>
+            </Form.Label>
+            <Form.Control
+              required
+              type="number"
+              placeholder="Price"
+              min="0"
+              max={projectDetails.project.price || 0}
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+          </Form.Group>
+        </Row>
+
+        <Button type="submit" variant="primary" className="my-2">
+          SAVE
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 }
 
-export default NewPhaseScreen
+export default NewPhaseScreen;
